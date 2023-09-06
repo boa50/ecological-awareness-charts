57d403254903db24fb4946aa323a99265d119c5d
boa50 <bruno.albuquerque50@gmail.com>
2023-09-06 17:34:40 +0930

    Added the continent as a category to the countries in the dataset

--- ecological-footprint/etl/data_manipulation.py
+++ ecological-footprint/etl/data_manipulation.py
@@ -34,49 +34,54 @@ for file_name in os.listdir(zip_temp_path):
 pop_file_path = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "zip_temp/" + pop_file_name
 )
-
 pop_df = pd.read_csv(pop_file_path, skiprows=4)
-countries_df = pd.read_csv(
-    "https://gist.githubusercontent.com/tadast/8827699/raw/f5cac3d42d16b78348610fc4ec301e9234f82821/countries_codes_and_coordinates.csv"
+
+countries_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "countries_continents.csv"
 )
+countries_df = pd.read_csv(countries_file_path)
 
 pop_df = pop_df[["Country Name", "Country Code", "2022"]]
-countries_df = countries_df[["Alpha-2 code", "Alpha-3 code"]]
-countries_df["Country Code"] = (
-    countries_df["Alpha-3 code"].str.replace('"', "").str.strip()
-)
-countries_df["isoa2"] = countries_df["Alpha-2 code"].str.replace('"', "").str.strip()
+countries_df["Country Code"] = countries_df["ISO3166-1-Alpha-3"].str.strip()
+countries_df["isoa2"] = countries_df["ISO3166-1-Alpha-2"].str.strip()
+countries_df["Continent"] = countries_df["Continent"].fillna("NA")
 
 filtered_df = pd.merge(pop_df, countries_df, on="Country Code", how="left")
 filtered_df = filtered_df[
-    (filtered_df["Alpha-2 code"].notna()) & (filtered_df["2022"] > 10e5)
+    (filtered_df["ISO3166-1-Alpha-2"].notna()) & (filtered_df["2022"] > 10e5)
 ]
-
 
 file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), "countries.json")
 with open(file_name, encoding="utf-8") as f:
     countries = json.load(f)
 
-filtered_countries = []
-for country in countries:
-    if not filtered_df[filtered_df["isoa2"] == country["isoa2"]].empty:
-        filtered_countries.append(int(country["countryCode"]))
+filtered_countries = [
+    (int(country["countryCode"]))
+    for country in countries
+    if not filtered_df[filtered_df["isoa2"] == country["isoa2"]].empty
+]
 
+country_continent = {}
+for country in countries:
+    if not countries_df[countries_df["isoa2"] == country["isoa2"]].empty:
+        country_continent[int(country["countryCode"])] = countries_df[
+            countries_df["isoa2"] == country["isoa2"]
+        ].iloc[0]["Continent"]
 
 file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), "earth_data.json")
 with open(file_name, encoding="utf-8") as f:
     earth_data = json.load(f)
 
-dataset = []
-for element in earth_data:
-    if (element["countryCode"] in filtered_countries) & (element["value"] > 0):
-        dataset.append(
-            {
-                "date": str(element["year"]) + "-01-01",
-                "name": element["shortName"],
-                "value": element["value"],
-            }
-        )
+dataset = [
+    {
+        "date": str(x["year"]) + "-01-01",
+        "name": x["shortName"],
+        "category": country_continent[x["countryCode"]],
+        "value": x["value"],
+    }
+    for x in earth_data
+    if (x["countryCode"] in filtered_countries) & (x["value"] > 0)
+]
 
 save_file(dataset, "dataset.json")
 

f8ea84662295d956839ab0764c3fdb627de260d0
boa50 <bruno.albuquerque50@gmail.com>
2023-10-25 10:47:49 -0300

    Added animation to the co2 bubbles on the GDP map chart

--- gdp/main.js
+++ gdp/main.js
@@ -6,7 +6,7 @@ const getData = async () =>
         d3.csv('./data/dataset.csv', d => { data.set(d['Country Code'], d) }),
         d3.csv('./data/dataset.csv'),
         d3.csv('./data/co2-emissions.csv'),
-        d3.csv('./data/centroids.csv', d => { centroids.set(d.code, [+d.latitude, +d.longitude])})
+        d3.csv('./data/centroids.csv', d => { centroids.set(d.code, [+d.latitude, +d.longitude]) })
     ])
 
 const width = 600
@@ -25,7 +25,7 @@ const path = d3
 
 const getLastGdp = (countryCode, year) => {
     if (data.get(countryCode)) {
-        if(!data.get(countryCode)[year]) {
+        if (!data.get(countryCode)[year]) {
             return year == minYear ? 0 : getLastGdp(countryCode, year - 1)
         }
 
@@ -48,7 +48,7 @@ const getProjection = countryCode => {
 getData().then(dataset => {
     const geo = dataset[0]
     const fullData = dataset[2]
-    const co2 = dataset[3].filter(d => (d.Code !== '') && (d.Year == 2021))
+    const co2 = dataset[3].filter(d => (d.Code !== ''))
     const years = Array.from({ length: maxYear - minYear + 1 }, (_, i) => i + minYear)
 
     const colour = d3
@@ -66,6 +66,10 @@ getData().then(dataset => {
         })(fullData, years)))
         .interpolator(d3.interpolateGreens)
 
+    const circleSize = d3
+        .scaleLinear()
+        .domain(d3.extent(co2, d => +d.Emissions))
+        .range([1, 100])
 
     const svg = d3
         .select('#mapChart')
@@ -96,6 +100,17 @@ getData().then(dataset => {
                 .attr('d', path)
                 .attr('fill', d => colour(getLastGdp(d.id, year)))
 
+            svg
+                .selectAll('.co2Circles')
+                .data(co2.filter(d => d.Year == year))
+                .join('circle')
+                .classed('co2Circles', true)
+                .attr('cx', d => getProjection(d.Code)[0])
+                .attr('cy', d => getProjection(d.Code)[1])
+                .attr('r', d => circleSize(d.Emissions))
+                .style('fill', d3.schemeReds[7][3])
+                .attr('fill-opacity', .7)
+
             transition.end().then(() => ticker.text(year))
 
             await transition.end()
@@ -103,20 +118,4 @@ getData().then(dataset => {
     }
 
     chart()
-
-
-    const circleSize = d3
-        .scaleLinear()
-        .domain(d3.extent(co2, d => +d.Emissions))
-        .range([1, 50])
-
-    svg
-        .selectAll('.co2Circles')
-        .data(co2)
-        .join('circle')
-        .attr('cx', d => getProjection(d.Code)[0])
-        .attr('cy', d => getProjection(d.Code)[1])
-        .attr('r', d => circleSize(d.Emissions))
-        .style('fill', d3.schemeReds[7][3])
-        .attr('fill-opacity', .7)
 })
